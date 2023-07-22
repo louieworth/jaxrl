885a544f90fbade9dd13b70ff222a3db19e9444d
louieworth <jiangli3859@gmail.com>
2023-07-22 12:10:55 +0800

    <fetch> sparse

--- jaxrl/agents/sac/sac_learner.py
+++ jaxrl/agents/sac/sac_learner.py
@@ -1,5 +1,4 @@
 """Implementations of algorithms for continuous control."""
-
 import functools
 from typing import Optional, Sequence, Tuple
 
@@ -7,6 +6,7 @@ import jax
 import jax.numpy as jnp
 import numpy as np
 import optax
+import jaxpruner
 
 from jaxrl.agents.sac import temperature
 from jaxrl.agents.sac.actor import update as update_actor
@@ -17,15 +17,27 @@ from jaxrl.networks import critic_net, policies
 from jaxrl.networks.common import InfoDict, Model, PRNGKey
 
 
-@functools.partial(jax.jit,
-                   static_argnames=('backup_entropy', 'update_target'))
+# @functools.partial(jax.jit,
+#                    static_argnames=('backup_entropy', 'update_target'))
 def _update_jit(
-    rng: PRNGKey, actor: Model, critic: Model, target_critic: Model,
-    temp: Model, batch: Batch, discount: float, tau: float,
+    rng: PRNGKey, actor: Model, actor_pruner, critic: Model, critic_pruner: Model, 
+    target_critic: Model, temp: Model, batch: Batch, discount: float, tau: float,
     target_entropy: float, backup_entropy: bool, update_target: bool
 ) -> Tuple[PRNGKey, Model, Model, Model, Model, InfoDict]:
-
+    
+    # TODO: 他是直接apply mask还是通过直接改变weight得方法dynamic weight rescaling (layer normalization)
     rng, key = jax.random.split(rng)
+    is_ste = isinstance(actor_pruner, (jaxpruner.SteMagnitudePruning,
+                                         jaxpruner.SteRandomPruning))
+    if is_ste:
+        critic_pre_op = jax.jit(actor_pruner.pre_forward_update)   
+        critic_new_params = critic_pre_op(actor.params, actor.opt_state)
+        critic = critic.replace(params=critic_new_params)
+        
+        actor_pre_op = jax.jit(actor_pruner.pre_forward_update)   
+        actor_new_params = actor_pre_op(actor.params, actor.opt_state)
+        actor = actor.replace(params=actor_new_params)
+        
     new_critic, critic_info = update_critic(key,
                                             actor,
                                             critic,
@@ -34,20 +46,46 @@ def _update_jit(
                                             batch,
                                             discount,
                                             backup_entropy=backup_entropy)
+    # TODO apply mask一般是post_gradient_update 还是 pre_forward_update？有什么样的区别？什么时候是apply mask，什么时候是apply gradient？
+    # Apply masks to parameters.
+    post_critic_params = critic_pruner.post_gradient_update(
+        new_critic.params, new_critic.opt_state)
+    new_critic = new_critic.replace(params=post_critic_params)
+
+    rng, key = jax.random.split(rng)
+    new_actor, actor_info = update_actor(key, actor, new_critic, temp, batch)
+    post_actor_params = actor_pruner.post_gradient_update(
+    new_actor.params, new_actor.opt_state)
+    new_actor = new_actor.replace(params=post_actor_params)
+    
+    new_temp, alpha_info = temperature.update(temp, actor_info['entropy'],
+                                              target_entropy)
+    
     if update_target:
         new_target_critic = target_update(new_critic, target_critic, tau)
     else:
         new_target_critic = target_critic
-
-    rng, key = jax.random.split(rng)
-    new_actor, actor_info = update_actor(key, actor, new_critic, temp, batch)
-    new_temp, alpha_info = temperature.update(temp, actor_info['entropy'],
-                                              target_entropy)
-
+    
+    sparsity_info = {}
+    if is_ste:
+        actor_sparsity = jaxpruner.summarize_sparsity(actor_new_params)
+        critic_sparsity = jaxpruner.summarize_sparsity(critic_new_params)
+    else:
+        actor_sparsity = jaxpruner.summarize_sparsity(new_actor.params)
+        critic_sparsity = jaxpruner.summarize_sparsity(new_critic.params)
+    sparsity_info['actor_sparsity'] = actor_sparsity['_total_sparsity']
+    # sparsity_info['actor_sparsity_layer_0']=actor_sparsity['MLP_0/Dense_0/kernel']
+    # sparsity_info['actor_sparsity_layer_1']=actor_sparsity['MLP_0/Dense_1/kernel']
+    sparsity_info['critic_sparsity'] = critic_sparsity['_total_sparsity']
+    # sparsity_info['critic_sparsity_layer_0']=critic_sparsity['VmapCritic_0/MLP_0/Dense_0/kernel']
+    # sparsity_info['critic_sparsity_layer_1']=critic_sparsity['VmapCritic_0/MLP_0/Dense_1/kernel']
+    # sparsity_info['critic_sparsity_layer_2']=critic_sparsity['VmapCritic_0/MLP_0/Dense_2/kernel']
+    # sparsity_info['critic_sparsity_dense_2']=critic_sparsity['Dense_2/kernel']
     return rng, new_actor, new_critic, new_target_critic, new_temp, {
         **critic_info,
         **actor_info,
-        **alpha_info
+        **alpha_info,
+        **sparsity_info
     }
 
 
@@ -68,7 +106,10 @@ class SACLearner(object):
                  backup_entropy: bool = True,
                  init_temperature: float = 1.0,
                  init_mean: Optional[np.ndarray] = None,
-                 policy_final_fc_init_scale: float = 1.0):
+                 policy_final_fc_init_scale: float = 1.0,
+                 layer_normalization: bool = False,
+                 actor_pruner: jaxpruner.algorithms = None,
+                 critic_pruner: jaxpruner.algorithms = None):
         """
         An implementation of the version of Soft-Actor-Critic described in https://arxiv.org/abs/1812.05905
         """
@@ -92,15 +133,16 @@ class SACLearner(object):
             hidden_dims,
             action_dim,
             init_mean=init_mean,
+            layer_normalization=layer_normalization,
             final_fc_init_scale=policy_final_fc_init_scale)
         actor = Model.create(actor_def,
                              inputs=[actor_key, observations],
-                             tx=optax.adam(learning_rate=actor_lr))
+                             tx=actor_pruner.wrap_optax(optax.adam(learning_rate=actor_lr)))
 
-        critic_def = critic_net.DoubleCritic(hidden_dims)
+        critic_def = critic_net.DoubleCritic(hidden_dims, layer_normalization=layer_normalization)
         critic = Model.create(critic_def,
                               inputs=[critic_key, observations, actions],
-                              tx=optax.adam(learning_rate=critic_lr))
+                              tx=critic_pruner.wrap_optax(optax.adam(learning_rate=critic_lr)))
         target_critic = Model.create(
             critic_def, inputs=[critic_key, observations, actions])
 
@@ -108,6 +150,8 @@ class SACLearner(object):
                             inputs=[temp_key],
                             tx=optax.adam(learning_rate=temp_lr))
 
+        self.actor_pruner = actor_pruner
+        self.critic_pruner = critic_pruner
         self.actor = actor
         self.critic = critic
         self.target_critic = target_critic
@@ -131,8 +175,8 @@ class SACLearner(object):
         self.step += 1
 
         new_rng, new_actor, new_critic, new_target_critic, new_temp, info = _update_jit(
-            self.rng, self.actor, self.critic, self.target_critic, self.temp,
-            batch, self.discount, self.tau, self.target_entropy,
+            self.rng, self.actor, self.actor_pruner, self.critic,  self.critic_pruner, 
+            self.target_critic, self.temp, batch, self.discount, self.tau, self.target_entropy,
             self.backup_entropy, self.step % self.target_update_period == 0)
 
         self.rng = new_rng
