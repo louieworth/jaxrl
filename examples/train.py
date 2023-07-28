f92bc403c0d98e732d7aca5aff265694f61d9f79
louieworth <jiangli3859@gmail.com>
2023-07-28 20:32:17 +0800

    <sync>

--- examples/train.py
+++ examples/train.py
@@ -28,7 +28,7 @@ flags.DEFINE_integer('seed', 42, 'Random seed.')
 flags.DEFINE_integer('eval_episodes', 10,
                      'Number of episodes used for evaluation.')
 flags.DEFINE_integer('log_interval', 1000, 'Logging interval.')
-flags.DEFINE_integer('eval_interval', 5000, 'Eval interval.')
+flags.DEFINE_integer('eval_interval', int(1e4), 'Eval interval.')
 flags.DEFINE_integer('batch_size', 256, 'Mini batch size.')
 flags.DEFINE_integer('updates_per_step', 1, 'Gradient updates per step.')
 flags.DEFINE_integer('max_steps', int(1e6), 'Number of training steps.')
@@ -36,16 +36,15 @@ flags.DEFINE_integer('start_training', int(1e4),
                      'Number of training steps to start training.')
 flags.DEFINE_boolean('tqdm', True, 'Use tqdm progress bar.')
 flags.DEFINE_boolean('save_video', False, 'Save videos during evaluation.')
-flags.DEFINE_boolean('track', True, 'Track experiments with Weights and Biases.')
+flags.DEFINE_boolean('track', False, 'Track experiments with Weights and Biases.')
 flags.DEFINE_string('wandb_project_name', "sparse_rl", "The wandb's project name.")
 flags.DEFINE_string('wandb_entity', "louis_t0", "the entity (team) of wandb's project")
-flags.DEFINE_boolean('negative_weight', True, 'Whether to calculate the negative weight')
-flags.DEFINE_integer('negative_weight_interval', int(2e4), 'Interval to calculate the negative weight')
 
-flags.DEFINE_boolean('save_model', True, 'Save model during training.')
+flags.DEFINE_boolean('save_model', False, 'Save model during training.')
 flags.DEFINE_integer('save_model_interval', int(1e6), 'Save model interval.')
 flags.DEFINE_boolean('load_model', False, 'Load model during training.')
-flags.DEFINE_integer('instant_sparsity_interval', int(1e4), 'Reset interval for the model.')
+flags.DEFINE_boolean('negative_side_variace', True, 'Whether to calculate the negative side variance')
+# flags.DEFINE_integer('instant_sparsity_interval', int(1e4), 'Reset interval for the model.')
 
 ###sparsity config
 flags.DEFINE_float("init_sparsity", 0.1, "initial sparsity")
@@ -142,6 +141,9 @@ def main(_):
     replay_buffer = ReplayBuffer(env.observation_space, env.action_space,
                                  replay_buffer_size or FLAGS.max_steps)
 
+    sparsity_distribution = functools.partial(
+                jaxpruner.sparsity_distributions.uniform, sparsity=FLAGS.init_sparsity)
+    pruner = jaxpruner.MagnitudePruning(sparsity_distribution_fn=sparsity_distribution)
     observation, done = env.reset(), False
     for i in tqdm.tqdm(range(1, FLAGS.max_steps + 1),
                        smoothing=0.1,
@@ -167,31 +169,48 @@ def main(_):
             for _ in range(FLAGS.updates_per_step):
                 batch = replay_buffer.sample(FLAGS.batch_size)
                 update_info = agent.update(batch)
-
-            if i % FLAGS.log_interval == 0:
-                wandb.log(update_info, step=i)
-        
-        # if i % FLAGS.instant_sparsity_interval == 0:
-        #     # first save the vanilla models
-        #     sparsity = FLAGS.init_sparsity + (
-        #         FLAGS.target_sparsity - FLAGS.init_sparsity) * i / FLAGS.max_steps
-        #     sparsity_distribution = functools.partial(
-        #         jaxpruner.sparsity_distributions.uniform, sparsity=sparsity)
-        #     pruner = jaxpruner.MagnitudePruning(sparsity_distribution_fn=sparsity_distribution)
-        #     agent.update_instant_sparsity(FLAGS.env_name, i, sparsity, pruner)
-        
         
         if i % FLAGS.eval_interval == 0:
+            sparsity = FLAGS.init_sparsity + (FLAGS.target_sparsity - FLAGS.init_sparsity) * i / FLAGS.max_steps
+            agent.update_instant_sparsity(FLAGS.env_name, i, sparsity, pruner)
             eval_stats = evaluate(agent, eval_env, FLAGS.eval_episodes, sparse_model=False)
             sparse_eval_stats = evaluate(agent, eval_env, FLAGS.eval_episodes, sparse_model=True)
             actor_sparsity_level = jaxpruner.summarize_sparsity(agent.copy_actor.params, only_total_sparsity=True)
             critic_sparsity_level = jaxpruner.summarize_sparsity(agent.copy_critic.params, only_total_sparsity=True)
-            wandb.log({'average_return': eval_stats['return']
-                        'sparse_average_return': sparse_eval_stats['return'],
-                        'actor_sparsity_level': actor_sparsity_level,
-                        'critic_sparsity_level': critic_sparsity_level}, step=i)
-            log.row({'normalized_return': eval_stats['return']})
-        
+            # wandb.log({'average_return': eval_stats['return'],
+            #             'sparse_average_return': sparse_eval_stats['return'],
+            #             'actor_sparsity_level': actor_sparsity_level,
+            #             'critic_sparsity_level': critic_sparsity_level}, step=i)
+            
+            # log.row({'normalized_return': eval_stats['return']})
+            if FLAGS.negative_side_variace:
+                actor_layer_0, actor_layer_1, actor_layer_2, actor_layer_3, actor_total = calculate_scores(agent.actor.params, negative_bias=True, is_actor=True)
+                critic_layer_0, critic_layer_1, critic_layer_2, critic_total = calculate_scores(agent.critic.params, negative_bias=True, is_actor=False)
+                sparse_actor_layer_0, sparse_actor_layer_1, sparse_actor_layer_2, sparse_actor_layer_3, sparse_actor_total = calculate_scores(agent.copy_actor.params, negative_bias=True, is_actor=True)
+                sparse_critic_layer_0, sparse_critic_layer_1, sparse_critic_layer_2, sparse_critic_total = calculate_scores(agent.copy_critic.params, negative_bias=True, is_actor=False)
+                log.row({'actor_layer_0': actor_layer_0,
+                         'sparse_actor_layer_0': sparse_actor_layer_0,
+                         'actor_layer_1': actor_layer_1,
+                         'sparse_actor_layer_1': sparse_actor_layer_1,
+                         'actor_layer_2': actor_layer_2,
+                         'sparse_actor_layer_2': sparse_actor_layer_2,
+                         'actor_layer_3': actor_layer_3,
+                         'sparse_actor_layer_3': sparse_actor_layer_3,
+                         'critic_layer_0': critic_layer_0,
+                         'sparse_critic_layer_0': sparse_critic_layer_0,
+                         'critic_layer_1': critic_layer_1,
+                         'sparse_critic_layer_1': sparse_critic_layer_1,
+                         'critic_layer_2': critic_layer_2,
+                         'sparse_critic_layer_2': sparse_critic_layer_2,
+                         'actor_total': actor_total,
+                         'sparse_actor_total': sparse_actor_total,
+                         'critic_total': critic_total,
+                         'sparse_critic_total': sparse_critic_total,
+                         'average_return': eval_stats['return'],
+                         'sparse_average_return': sparse_eval_stats['return'],
+                         'sparsity': sparsity
+                         })
+                
         if FLAGS.save_model and i % FLAGS.save_model_interval == 0:
             logging.info(f"save the model")
             agent.save_networks(FLAGS.env_name, additional_info=f"step_{i}")
--- run.bash
+++ run.bash
@@ -5,11 +5,11 @@
 # for env in "${envs[@]}"; do
 
 declare -a envs=("HalfCheetah-v4" "Hopper-v4" "Walker2d-v4" "Ant-v4" "walker-run" "cheetah-run" "fish-swim" "humanoid-stand" "humanoid-run" "quadruped-run" "hopper-hop")
-seeds=4
+seeds=5
 # XLA_PYTHON_CLIENT_MEM_FRACTION=0.15
 # for env in "${envs[@]}"; do
 for seed in $(seq 0 $((seeds-1))); do
-    python examples/train_max_sparsity.py \
+    CUDA_VISIBLE_DEVICES=1 python examples/train.py \
     --env_name="hopper-hop" \
     --seed=$seed
 done
