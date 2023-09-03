a7d74f582620b33bee1bccb9d62c06ae1cb0a3d5
louieworth <jiangli3859@gmail.com>
2023-09-03 22:37:11 +0800

    <update> .gitignore

--- run.bash
+++ run.bash
@@ -4,13 +4,13 @@ export XLA_PYTHON_CLIENT_MEM_FRACTION=0.1
 export WANDB_API_KEY=9d45bb78a65fb0f3b0402a9eae36ed832ae8cbdc
 GPU_LIST=(1)
 
-env_list=("HalfCheetah-v4" "Hopper-v4" "Walker2d-v4" "Ant-v4")
+env_list=("Hopper-v4" "Walker2d-v4" "Ant-v4")
 # mujoco: "HalfCheetah-v4" "Hopper-v4" "Walker2d-v4" "Ant-v4"
 # "walker-run" "cheetah-run" "acrobot-swingup" "finger-turn_hard" "fish-swim" 
 #  "humanoid-stand" "humanoid-run" "quadruped-run" "hopper-hop" "swimmer-swimmer15"
 
 # 
-for seed in 2; do
+for seed in 3; do
         for env in ${env_list[*]}; do
         GPU_DEVICE=${GPU_LIST[task%${#GPU_LIST[@]}]}
         CUDA_VISIBLE_DEVICES=$GPU_DEVICE 
